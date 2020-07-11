f5e802f274d069874835d8c67b309fda42d6cdad
Divyaank <divyaankt21@gmail.com>
2020-07-11 15:38:02 +0530

    Wrote test case using assertEquals()

--- src/test/java/com/divyaank/MathUtilsTest.java
+++ src/test/java/com/divyaank/MathUtilsTest.java
@@ -8,7 +8,12 @@ class MathUtilsTest {
 
 	@Test
 	void test() {
-		System.out.println("Test case ran!!!");
+		MathUtils mu = new MathUtils();
+		
+		int expected = 2;
+		int actual = mu.add(1,1);
+		
+		assertEquals(expected, actual);
 	}
 
 }
