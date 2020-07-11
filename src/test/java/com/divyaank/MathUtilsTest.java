e241f06d303583d5a327516198acb4682948d815
Divyaank <divyaankt21@gmail.com>
2020-07-11 16:17:18 +0530

    Added @BeforeEach life-cycle hook to initialize MathUtils object

--- src/test/java/com/divyaank/MathUtilsTest.java
+++ src/test/java/com/divyaank/MathUtilsTest.java
@@ -2,13 +2,20 @@ package com.divyaank;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MathUtilsTest {
 
+	private MathUtils mu;
+	
+	@BeforeEach
+	void initObj() {
+		mu = new MathUtils();
+	}
+	
 	@Test
 	void testAdd() {
-		MathUtils mu = new MathUtils();
 		int expected = 2;
 		int actual = mu.add(1,1);
 		assertEquals(expected, actual, "Add 2 numbers");
@@ -16,14 +23,12 @@ class MathUtilsTest {
 	
 	@Test
 	void testDivide() {
-		MathUtils mu = new MathUtils();
 		assertThrows(ArithmeticException.class, () -> mu.divide(1, 0), 
 				"Divide should throw ArithmeticException when denominator is zero");
 	}
 	
 	@Test 
 	void computeCircleArea() {
-		MathUtils mu = new MathUtils();
 		assertEquals(314.1592653589793, mu.computeCircleArea(10), 
 				"Should return right circle area");
 	}
