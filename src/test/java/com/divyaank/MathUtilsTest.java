48e274087dad5d338b71fa598c58d8db72dfed05
Divyaank <divyaankt21@gmail.com>
2020-07-11 15:58:16 +0530

    Added maven surefire plugin for automating test execution

--- src/test/java/com/divyaank/MathUtilsTest.java
+++ src/test/java/com/divyaank/MathUtilsTest.java
@@ -7,13 +7,17 @@ import org.junit.jupiter.api.Test;
 class MathUtilsTest {
 
 	@Test
-	void test() {
+	void testAdd() {
 		MathUtils mu = new MathUtils();
-		
 		int expected = 2;
 		int actual = mu.add(1,1);
-		
-		assertEquals(expected, actual);
+		assertEquals(expected, actual, "Add 2 numbers");
 	}
 
+	@Test 
+	void computeCircleArea() {
+		MathUtils mu = new MathUtils();
+		assertEquals(314.1592653589793, mu.computeCircleArea(10), 
+				"Should return right circle area");
+	}
 }
