f8bc0680baa8b9039d6181b3e8aa4a16139cddae
Divyaank <divyaankt21@gmail.com>
2020-07-11 16:00:15 +0530

    Added all the methods in MathUtils

--- src/main/java/com/divyaank/MathUtils.java
+++ src/main/java/com/divyaank/MathUtils.java
@@ -5,6 +5,18 @@ public class MathUtils {
 		return a + b;
 	}
 	
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+	
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	
+	public int divide(int a, int b) {
+		return a/b;
+	}
+	
 	public double computeCircleArea(double radius) {
 		return Math.PI * radius * radius;
 	}
