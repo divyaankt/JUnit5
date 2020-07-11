3dfec29535ee26cb7799806de2b9aec176358afb
Divyaank <divyaankt21@gmail.com>
2020-07-11 14:13:48 +0530

    Added MathUtilsTest class for MathUtils class

---
+++ src/main/java/com/divyaank/MathUtils.java
@@ -0,0 +1,7 @@
+package com.divyaank;
+
+public class MathUtils {
+	public int add(int a, int b) {
+		return a + b;
+	}
+}
---
+++ src/test/java/com/divyaank/MathUtilsTest.java
@@ -0,0 +1,14 @@
+package com.divyaank;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+	@Test
+	void test() {
+		System.out.println("Test case ran!!!");
+	}
+
+}
